3080f1c46b3cfaec02c58e850a84bceccbc127ed
KareemHesham1997 <kemo4devils@gmail.com>
2023-02-25 12:16:06 +0200

    commit

--- Terraform/vm.tf
+++ Terraform/vm.tf
@@ -5,30 +5,19 @@ resource "google_compute_instance" "private-vm" {
   zone         = "us-central1-a"
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "values"
+      }
     }
+
   }
+
   #connect it with management subnet
   network_interface {
     network = google_compute_network.kareem-vpc.id
     subnetwork = google_compute_subnetwork.management_subnet.id
   }
-  #Install kubectl, dockercli , google cloud auth plugin
-  metadata = {
-    startup-script = <<-EOF
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-    apt-get update -y && apt-get install ca-certificates curl gnupg lsb-release -y && mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-    sudo apt-get install -y apt-transport-https ca-certificates gnupg
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    sudo apt-get update && sudo apt-get install google-cloud-cli
-    apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-    EOF
-  }
+  
 }
 
